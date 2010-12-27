c99eca0c4a925da2b6cd9bd14df1f4b4c4b381f0
bi2one <excgate@gmail.com>
2010-12-27 21:36:06 +0900

    Finish 5th chapter

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,3 +8,5 @@ module ApplicationHelper
     end
   end
 end
+
+
--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,6 @@
 module PagesHelper
+  def logo
+    l = image_tag "logo.png", :alt => "Sample App", :class => "round"
+    link_to l, root_path
+  end
 end
